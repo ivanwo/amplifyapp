7f8aec2e6b6aeb57875378ceed17d58a4b036c3a
Ivan Wolyniec <ivanwolyniec@Eds-MacBook-Air-2.local>
2020-10-11 20:04:48 -0400

    added auth and changed the logo image out

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {withAuthenticator, AmplifySignOut} from '@aws-amplify/ui-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="auth.png" className="App-logo" alt="logo" />
+        <h1>We have added auth!</h1>
         <p>
           Whoa buddy, lots of work to be done here!
         </p>
       </header>
+      <AmplifySignOut />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
